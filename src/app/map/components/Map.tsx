af9e3afe5009849cc48c4d6ac92be4363298a146
Nat.p <otamnaz@gmail.com>
2025-03-25 11:08:23 +0700

    Edit error typescript

--- src/app/map/components/Map.tsx
+++ src/app/map/components/Map.tsx
@@ -9,9 +9,7 @@ import type { MapViewState } from "@deck.gl/core";
 import type { Tileset3D } from "@loaders.gl/tiles";
 import "maplibre-gl/dist/maplibre-gl.css";
 
-interface Props {}
-
-const MapLibre: React.FC<Props> = (_) => {
+const MapLibre: React.FC = () => {
   const [initialViewState, setInitialViewState] = React.useState<MapViewState>({
     longitude: 10,
     latitude: 50,
